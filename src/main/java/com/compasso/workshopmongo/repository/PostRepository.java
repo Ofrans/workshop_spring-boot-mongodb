b23f0b0ce34dfc891e1736f4ffb8160a8a253adc
Fransergio Filho <fransergio.morato.pb@compasso.com.br>
2024-09-06 16:58:57 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/compasso/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,12 @@
+package com.compasso.workshopmongo.repository;
+
+import com.compasso.workshopmongo.domain.Post;
+import com.compasso.workshopmongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+
+}
