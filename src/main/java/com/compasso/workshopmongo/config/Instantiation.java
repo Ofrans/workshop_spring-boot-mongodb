259e4f2d60528e9d6511c25577e2479cda929655
Fransergio Filho <fransergio.morato.pb@compasso.com.br>
2024-09-06 17:04:30 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/compasso/workshopmongo/config/Instantiation.java
+++ src/main/java/com/compasso/workshopmongo/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.compasso.workshopmongo.config;
 
 import com.compasso.workshopmongo.domain.Post;
 import com.compasso.workshopmongo.domain.User;
+import com.compasso.workshopmongo.dto.AuthorDTO;
 import com.compasso.workshopmongo.repository.PostRepository;
 import com.compasso.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,11 +34,11 @@ public class Instantiation implements CommandLineRunner {
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria );
-        Post post2 = new Post(null, sdf.parse("21/03/2018"), "Bom dia", "Acordei Feliz hoje!", maria );
-
-
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria) );
+        Post post2 = new Post(null, sdf.parse("21/03/2018"), "Bom dia", "Acordei Feliz hoje!", new AuthorDTO(maria) );
+
         postRepository.saveAll(Arrays.asList(post1, post2));
     }
 }
