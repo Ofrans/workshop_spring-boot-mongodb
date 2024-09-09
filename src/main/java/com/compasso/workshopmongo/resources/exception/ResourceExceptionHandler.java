e94c2e8425640986cf455f98086ad2831d408a0a
Fransergio Filho <fransergio.morato.pb@compasso.com.br>
2024-09-09 14:45:32 -0300

    Consulta com vários critérios

--- src/main/java/com/compasso/workshopmongo/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/compasso/workshopmongo/resources/exception/ResourceExceptionHandler.java
@@ -1,12 +1,13 @@
 package com.compasso.workshopmongo.resources.exception;
 
 import com.compasso.workshopmongo.services.exception.ObjectNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import javax.servlet.http.HttpServletRequest;
+
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
