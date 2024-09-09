93f2fc8d10a93f7098d72938e3a8a64f63778558
Fransergio Filho <fransergio.morato.pb@compasso.com.br>
2024-09-09 13:42:54 -0300

    Consulta simples com query methods

--- src/main/java/com/compasso/workshopmongo/resources/PostResource.java
+++ src/main/java/com/compasso/workshopmongo/resources/PostResource.java
@@ -2,15 +2,13 @@ package com.compasso.workshopmongo.resources;
 
 import com.compasso.workshopmongo.domain.Post;
 import com.compasso.workshopmongo.domain.Post;
+import com.compasso.workshopmongo.resources.util.URL;
 import com.compasso.workshopmongo.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.net.URI;
 import java.util.List;
-import java.util.stream.Collectors;
 
 
 @RestController
@@ -24,6 +22,14 @@ public class PostResource {
     public ResponseEntity<Post> findById(@PathVariable String id) {
         Post post = service.findById(id);
         return ResponseEntity.ok().body(post);
+    }
+
+    @RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+        text = URL.deCodeParam(text);
+        List<Post> posts = service.findByTitle(text);
+
+        return ResponseEntity.ok().body(posts);
     }
 
 }
